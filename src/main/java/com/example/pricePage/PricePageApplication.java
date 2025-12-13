2e9683080df3fd9c6935288145858f21cedc851c
김서진 <98688397+seojinkim123@users.noreply.github.com>
2025-12-13 18:14:34 +0900

    first?

---
+++ src/main/java/com/example/pricePage/PricePageApplication.java
@@ -0,0 +1,13 @@
+package com.example.pricePage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricePageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pricePage/PricePageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pricePage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
